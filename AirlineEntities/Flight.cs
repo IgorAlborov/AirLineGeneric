cddd5bfb2b23a7604c20db2ad0400defb5cc2e32
i.alborov <i.alborov@hotmail.com>
2016-04-11 16:05:37 +0300

    Внесены entities

---
+++ AirlineEntities/Flight.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace AirlineEntities
+{
+    public class Flight
+    {
+        //констанкта кол-ва пассажиров в самолете
+        public static readonly int FlightCapasity;
+        static Flight() {
+            FlightCapasity = 5;
+        }
+        enum DirectionForFlight
+        {
+            Arrival, Departure
+        }
+        enum StatusForFlight
+        {
+            CheckIn, GateClosed, Arrived, DepartedAt, Unknown, Canceled, ExpectedAt, Delayed, InFlight
+
+        }
+        DirectionForFlight FlightDirection;
+        DateTime FlightDate { get; set; }
+        public int? FlightNumber { get; private set; }
+        string FlightCity { get; set; }
+        private char _flightTerminal;
+        public char FlightTerminal {
+            get { return _flightTerminal; }
+            set {
+                if ("ABCDEFG".Contains(value))
+                    _flightTerminal = value;
+            }
+        }
+        private byte _flightGate;
+        public byte FlightGate {
+            get { return _flightGate; }
+            set {
+                if ((value > 0) && (value < 10))
+                    _flightGate = value;
+            }
+        }
+        StatusForFlight FlightStatus;
+        public decimal FlightPriceBussiness;
+        public decimal FlightPriceEconomy;
+        Passenger[] PassengersList;
+        public byte FlightFreePlace {
+            get {
+                byte i = 0;
+                foreach (Passenger item in PassengersList) {
+                    if (item == null)
+                        i++;
+                }
+                return i;
+            }
+        }
+
+        public Flight() {
+            PassengersList = new Passenger[FlightCapasity];
+
+        }
+
+        public override string ToString() {
+            int i = FlightCapasity - 5;
+            return String.Format("|{0,-9}|{1,-16}|{2,-6}|{3,-15}|{4,11}/{5}|{6,-10}|{7,11}|", FlightDirection,
+                   FlightDate.ToString("dd-MM-yyyy hh:mm"), FlightNumber, FlightCity, FlightTerminal, FlightGate,
+                   FlightStatus, FlightFreePlace);
+        }
+
+        public Flight(byte flightDirection, DateTime flightDate, int flightNumber, string flightCity,
+            char flightTerminal, int flightGate, byte flightStatus, int flightPriceBusiness,
+            int flightPriceEconomy) : this() {
+            FlightDirection = (DirectionForFlight)flightDirection;
+            FlightDate = flightDate;
+            FlightNumber = flightNumber;
+            FlightCity = flightCity;
+            FlightTerminal = flightTerminal;
+            FlightGate = (byte)flightGate;
+            FlightStatus = (StatusForFlight)flightStatus;
+            FlightPriceBussiness = (decimal)flightPriceBusiness;
+            FlightPriceEconomy = (decimal)flightPriceEconomy;
+        }
+
+        #region insert sample value
+        public Flight(string sample) : this() {
+            if (sample == "sample") {
+                Random rnd = new Random((int)DateTime.Now.Ticks);
+                string[] Citys = { "Kyiv", "Moscow", "London", "Rim", "Minsk", "Antalia", "Warshava" };
+                string terminals = "ABCDEFG";
+                FlightDirection = (DirectionForFlight)rnd.Next(0, 1);
+                Thread.Sleep(50);
+                FlightDate = DateTime.Now.AddMinutes(rnd.Next(300));
+                Thread.Sleep(50);
+                FlightNumber = rnd.Next(100, 999);
+                Thread.Sleep(50);
+                FlightCity = Citys[rnd.Next(0, Citys.Length)];
+                Thread.Sleep(50);
+                FlightTerminal = terminals[rnd.Next(0, terminals.Length)];
+                Thread.Sleep(50);
+                FlightGate = (byte)rnd.Next(1, 9);
+                Thread.Sleep(50);
+                FlightStatus = (StatusForFlight)rnd.Next(0, 8);
+                Thread.Sleep(50);
+                FlightPriceBussiness = (decimal)rnd.Next(300, 1000);
+                Thread.Sleep(50);
+                FlightPriceEconomy = (decimal)rnd.Next(80, 290);
+                Thread.Sleep(50);
+                int tempPassengerCount = rnd.Next(1, 5);
+                for (int i = 0; i < tempPassengerCount; i++) {
+                    Thread.Sleep(100);
+                    int MaleIndex = rnd.Next(1, 1000) % 2;
+                    switch (MaleIndex) {
+                        case 0:      //Male
+                            PassengersList[i] = new Passenger("male", FlightPriceBussiness, FlightPriceEconomy);
+                            break;
+                        case 1:     //Female
+                            PassengersList[i] = new Passenger("female", FlightPriceBussiness, FlightPriceEconomy);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        public void ChangeStatus(byte statusIndex) {
+            if (statusIndex >= 0 && statusIndex <= 8)
+                this.FlightStatus = (StatusForFlight)statusIndex;
+        }
+
+        public string GetFlightStatus() {
+            return this.FlightStatus.ToString();
+        }
+
+    }
+}
+
---
+++ AirlineEntities/Passenger.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineEntities
+{
+    enum ClassForTicket { Business, Economy }
+    struct Ticket
+    {
+        ClassForTicket TicketClass;
+        decimal TicketPrice;
+        public Ticket(ClassForTicket ticketClass, decimal ticketPrice) {
+            TicketClass = ticketClass;
+            TicketPrice = ticketPrice;
+        }
+        public override string ToString() {
+            if (TicketClass == ClassForTicket.Business)
+                return String.Format("Business|{0,13:C}", TicketPrice);
+            else
+                return String.Format("Economy |{0,13:C}", TicketPrice);
+        }
+    }
+    class Passenger
+    {
+        enum SexForPassenger { Male, Female }
+        private string _passengerFirstname;
+        public string PassengerFirstname {
+            get { return _passengerFirstname; }
+            set {
+                if (value != null)
+                    _passengerFirstname = value;
+            }
+        }
+        private string _passengerLastname;
+        public string PassengerLastname {
+            get { return _passengerLastname; }
+            set {
+                if (value != null)
+                    _passengerLastname = value;
+            }
+        }
+        string PassengerNationality { get; set; }
+        private string _passengerPasspower;
+        public string PassengerPasspower {
+            get { return _passengerPasspower; }
+            private set { _passengerPasspower = value; }
+        }
+        private DateTime _passengerBirthday;
+        public DateTime PassengerBirthday {
+            get { return _passengerBirthday; }
+            set {
+                if (value != null && value < DateTime.Now)
+                    _passengerBirthday = value;
+            }
+        }
+        SexForPassenger PassengerSex;
+        Ticket PassengerTicket;
+
+        public override string ToString() {
+            return String.Format("|{0,-14}|{1,-16}|{2,-10}|{3,-8}|{4,7}|{5,-11}|{6,10}|", PassengerFirstname,
+                   PassengerLastname, PassengerBirthday.ToString("dd-MM-yyyy"), PassengerPasspower, PassengerSex,
+                   PassengerNationality, PassengerTicket.ToString());
+        }
+
+        #region insert sample value
+        public Passenger(string sex, decimal bussinesTicket, decimal economTicket) {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            string[] MaleFirstName = { "Rufus", "Bear", "Dakota", "Fido",
+                                "Vanya", "Samuel", "Koani", "Volodya"};
+            string[] FemaleFirstName = { "Maggie", "Penny", "Saya", "Princess",
+                                  "Abby", "Laila", "Sadie", "Olivia" };
+            string[] MaleLastName = { "Ivanov", "Petrov", "Sidorov", "Kafa",
+                                "Grusha", "Vorobey", "Grishko"};
+            string[] FemaleLastName = { "Ivanova", "Petrova", "Sidorova", "Gracheva",
+                                  "Lastochkina", "Krasivaya"};
+            string[] Nationality = { "Ukrainian", "Russian", "German", "Serbian", "Balerusian", "Slovak" };
+            string[] SerialPassport = { "EA", "GB", "MK", "QW", "ED" };
+            switch (sex) {
+                case "male":
+                    PassengerFirstname = MaleFirstName[rnd.Next(0, MaleFirstName.Length)];
+                    PassengerLastname = MaleLastName[rnd.Next(0, MaleLastName.Length)];
+                    PassengerNationality = Nationality[rnd.Next(0, Nationality.Length)];
+                    PassengerPasspower = SerialPassport[rnd.Next(0, SerialPassport.Length)] + rnd.Next(111111, 999999).ToString();
+                    PassengerBirthday = DateTime.Now.AddDays(-rnd.Next(7000, 22000));
+                    PassengerSex = SexForPassenger.Male;
+                    if ((rnd.Next(1, 1000) % 2) == 0)
+                        PassengerTicket = new Ticket(ClassForTicket.Business, bussinesTicket);
+                    else
+                        PassengerTicket = new Ticket(ClassForTicket.Economy, economTicket);
+                    break;
+                case "female":
+                    PassengerFirstname = FemaleFirstName[rnd.Next(0, FemaleFirstName.Length)];
+                    PassengerLastname = FemaleLastName[rnd.Next(0, FemaleLastName.Length)];
+                    PassengerNationality = Nationality[rnd.Next(0, Nationality.Length)];
+                    PassengerPasspower = SerialPassport[rnd.Next(0, SerialPassport.Length)] + rnd.Next(111111, 999999).ToString();
+                    PassengerBirthday = DateTime.Now.AddDays(-rnd.Next(7000, 22000));
+                    PassengerSex = SexForPassenger.Female;
+                    if ((rnd.Next(1, 1000) % 2) == 0)
+                        PassengerTicket = new Ticket(ClassForTicket.Business, bussinesTicket);
+                    else
+                        PassengerTicket = new Ticket(ClassForTicket.Economy, economTicket);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        public Passenger(string passengerFirstname, string passengerLastname,
+                string passengerNationality, string passengerPasspower, DateTime passengerBirthday,
+                int passengerSex, int passengerTicket, decimal priceBusiness, decimal priceEconomy) {
+            PassengerFirstname = passengerFirstname;
+            PassengerLastname = passengerLastname;
+            PassengerNationality = passengerNationality;
+            PassengerPasspower = passengerPasspower;
+            PassengerBirthday = passengerBirthday;
+            PassengerSex = (SexForPassenger)passengerSex;
+            if (passengerTicket == 0)
+                PassengerTicket = new Ticket(ClassForTicket.Business, priceBusiness);
+            else
+                PassengerTicket = new Ticket(ClassForTicket.Economy, priceEconomy);
+        }
+
+
+    }
+}
